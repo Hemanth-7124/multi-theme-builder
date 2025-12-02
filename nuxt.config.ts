2eaa9f3c86fa045a1e3a87f20a7cf5f1a2dfd3b3
Hemanth Kumar <hemanth.k9500@gmail.com>
2025-12-02 17:21:33 +0530

    feat: changes added properly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  css: [
+    '~/assets/css/main.css'
+  ],
 
-  runtimeConfig: {
-    public: {
-      appName: 'Brand Router Engine'
-    }
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  nitro: {
+    preset: 'netlify'
   }
 })
+
